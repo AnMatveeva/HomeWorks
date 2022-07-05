2a98c65d157457a532aeb9c5f7589362c1b3bf3e
Anastasia <a.n.matveeva99@gmail.com>
2022-07-05 20:12:24 +0300

    Changes added js file to 3 tasks

---
+++ lesson003/task1/file.js
@@ -0,0 +1,7 @@
+let userName = 'John';
+let number = 25;
+number -= 1;
+
+console.log(userName);
+console.log(number);
+        
---
+++ lesson003/task2/file.js
@@ -0,0 +1,11 @@
+let nameStore = 'Supermarket';
+console.log(nameStore);
+
+const storeDescription = {
+budget : 10000,
+employees : ['Julia', 'Kate', 'John'],
+products : ('meat: 5$; ') + (' vegetables: 5$;'),
+open : open || close 
+};
+
+console.log(storeDescription);
